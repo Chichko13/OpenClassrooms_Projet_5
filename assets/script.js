cf11e144b0c330db5f5985915872fd51deea1ada
Chichko <134549448+Chichko13@users.noreply.github.com>
2023-10-09 23:55:45 +0200

    Projet 5 OC

---
+++ assets/script.js
@@ -0,0 +1,70 @@
+const slides = [
+	{
+		"image":"./assets/images/slideshow/slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+// Var
+const arrowLeft = document.querySelector(".arrow_left")
+const arrowRight = document.querySelector(".arrow_right")
+const dots = document.querySelector(".dots")
+
+arrowLeft.addEventListener("click",clickArrowLeft)
+arrowRight.addEventListener("click",clickArrowRight)
+
+for (let bullet = 0; bullet < slides.length; bullet++){
+		const dot = document.createElement("div")
+		dot.className = "dot"
+		dots.append(dot)
+}
+
+const dotAll = document.querySelectorAll(".dot")
+dotAll[0].classList.add("dot_selected")
+
+let tagLine = document.querySelector("#banner p")
+let imgBanner = document.querySelector("#banner .banner-img")
+let currentSlide = 0
+// -------------------------------------------------FUNCTION----------------------------------------------------------------
+
+
+
+function dotMove(){
+	for (let i = 0; i < dotAll.length; i++ ){
+		dotAll[i].classList.remove("dot_selected")
+	}
+    dotAll[currentSlide].classList.add("dot_selected");
+}
+
+
+function clickArrowRight(){
+	currentSlide++
+	if(currentSlide === slides.length){
+		currentSlide = 0
+		}
+		imgBanner.src = slides[currentSlide].image
+		tagLine.innerHTML = slides[currentSlide].tagLine
+		dotMove()
+}
+
+function clickArrowLeft(){
+	currentSlide--
+	if(currentSlide < 0){
+		currentSlide = slides.length -1
+	}
+		console.log(currentSlide)
+		tagLine.innerHTML = slides[currentSlide].tagLine
+		imgBanner.src = slides[currentSlide].image
+		dotMove()
+}
